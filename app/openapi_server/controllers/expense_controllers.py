867ba8ad73e340e93095b41207e3d6a6856ae00c
VWT <max.vanderweide@student.hu.nl>
2019-07-05 14:20:34 +0200

    Added: openapi.yaml and functions to expense_controllers.py

--- app/openapi_server/controllers/expense_controllers.py
+++ app/openapi_server/controllers/expense_controllers.py
@@ -1,1 +1,45 @@
-# Add Your controllers here
+import json
+
+from flask import request, jsonify
+from google.cloud import datastore
+
+
+def getExpenses(expense_id):
+    print('WorkingID: ',  expense_id)
+    isUser = expense_id > 0
+    return expense_id, (200 if isUser else 404)
+
+def addExpenses(expenses_id):
+    expenses_id = str(expenses_id)
+    erin = "test" in expenses_id
+    return expenses_id, (200 if erin else 404)
+
+def getAttachmentsById(expenses_id):
+    expenses_id = str(expenses_id)
+    erin = "test" in expenses_id
+    return expenses_id, (200 if erin else 404)
+
+def updateAttachmentsById(expenses_id):
+    expenses_id = str(expenses_id)
+    erin = "test" in expenses_id
+    return expenses_id, (200 if erin else 404)
+
+def deleteAttachmentsById(expenses_id):
+    expenses_id = str(expenses_id)
+    erin = "test" in expenses_id
+    return expenses_id, (200 if erin else 404)
+
+def addAttachments(expenses_id):
+    expenses_id = str(expenses_id)
+    erin = "test" in expenses_id
+    return expenses_id, (200 if erin else 404)
+
+def getDocumentById(expenses_id):
+    expenses_id = str(expenses_id)
+    erin = "test" in expenses_id
+    return expenses_id, (200 if erin else 404)
+
+def addDocument(expenses_id):
+    expenses_id = str(expenses_id)
+    erin = "test" in expenses_id
+    return expenses_id, (200 if erin else 404)
