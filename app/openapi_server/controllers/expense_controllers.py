36572ee3182639752fc26292ed9a76436ee26132
VWT <max.vanderweide@student.hu.nl>
2019-07-08 10:19:33 +0200

    Edit: Removed camel casing in expense_controllers

--- app/openapi_server/controllers/expense_controllers.py
+++ app/openapi_server/controllers/expense_controllers.py
@@ -1,45 +1,44 @@
-import json
-
 from flask import request, jsonify
 from google.cloud import datastore
 
 
-def getExpenses(expense_id):
-    print('WorkingID: ',  expense_id)
-    isUser = expense_id > 0
-    return (expense_id, 200) if isUser else 404
+def getexpenses(expense_id):
+    dsc = datastore.Client()
+    expenses_info = dsc.query(kind='Expenses').add_filter('id', '=', expense_id)
+    return jsonify(expenses_info.fetch())
 
-def addExpenses(expenses_id):
+def addexpenses(expenses_id):
     expenses_id = str(expenses_id)
     erin = "test" in expenses_id
     return expenses_id, (200 if erin else 404)
 
-def getAttachmentsById(expenses_id):
+def getattachmentsbyid(expenses_id):
     expenses_id = str(expenses_id)
     erin = "test" in expenses_id
     return expenses_id, (200 if erin else 404)
 
-def updateAttachmentsById(expenses_id):
+def updateattachmentsbyid(expenses_id):
     expenses_id = str(expenses_id)
     erin = "test" in expenses_id
     return expenses_id, (200 if erin else 404)
 
-def deleteAttachmentsById(expenses_id):
+def deleteattachmentsbyid(expenses_id):
     expenses_id = str(expenses_id)
     erin = "test" in expenses_id
     return expenses_id, (200 if erin else 404)
 
-def addAttachments(expenses_id):
+def addattachments(expenses_id):
     expenses_id = str(expenses_id)
     erin = "test" in expenses_id
     return expenses_id, (200 if erin else 404)
 
-def getDocumentById(expenses_id):
+def getdocumentbyid(expenses_id):
     expenses_id = str(expenses_id)
     erin = "test" in expenses_id
     return expenses_id, (200 if erin else 404)
 
-def addDocument(expenses_id):
+def adddocument(expenses_id):
     expenses_id = str(expenses_id)
     erin = "test" in expenses_id
     return expenses_id, (200 if erin else 404)
+
