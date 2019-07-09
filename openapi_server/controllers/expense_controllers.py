26ce0affe3b6703d1b1a77e57707ea316b6bf1e1
VWT <max.vanderweide@student.hu.nl>
2019-07-09 09:13:07 +0200

    Added controller functions to ClaimExpenses DAT-345

--- openapi_server/controllers/expense_controllers.py
+++ openapi_server/controllers/expense_controllers.py
@@ -1,4 +1,5 @@
 import connexion
+from flask import make_response, jsonify
 from google.cloud import datastore
 
 from openapi_server.models.documents import Documents
@@ -16,6 +17,43 @@ class ClaimExpenses:
     def __init__(self):
         self.db_client = datastore.Client()
         self.request = connexion.request
+
+    def get_expenses(self, expense_id):
+        """Get expenses with expense_id"""
+        expenses_info = self.db_client.query(kind='Expense') \
+            .add_filter('id', '=', expense_id)
+        expenses_data = expenses_info.fetch()
+
+        if expenses_data:
+            return jsonify(expenses_data)
+        else:
+            return make_response(jsonify(None), 204)
+
+    def get_all_expenses(self):
+        """Get JSON of all the expenses"""
+        expenses_info = self.db_client.query(kind='Expense')
+        expenses_data = expenses_info.fetch()
+
+        if expenses_data:
+            results = [{
+                'Title': ed.Title,
+                'Amount': ed.Amount
+            } for ed in expenses_data]
+            return jsonify(results)
+        else:
+            return make_response(jsonify(None), 204)
+
+    def add_expenses(self, data):
+        """Add expense with given data amount and given data note"""
+        key = self.db_client.key('Expense')
+        entity = datastore.Entity(key=key)
+        entity.update({
+            'ID': entity.key.id_or_name,
+            'Amount': data['amount'],
+            'Note': data['note']
+        })
+        self.db_client.put(entity)
+        return make_response(jsonify(entity.key.id_or_name), 201)
 
 
 expense_instance = ClaimExpenses()
