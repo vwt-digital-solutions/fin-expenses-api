758f55989d3adefc99763b1e2ba7703b7d0f31f6
Jelle <j.bouwens@vwt.digital>
2019-07-10 10:59:53 +0200

    Add: Database connectivity on controllers DAT-372

--- openapi_server/controllers/expense_controllers.py
+++ openapi_server/controllers/expense_controllers.py
@@ -18,26 +18,33 @@ class ClaimExpenses:
         self.db_client = datastore.Client()
         self.request = connexion.request
 
-    def get_expenses(self, expense_id):
+    def get_expenses(self, expenses_id):
         """Get expenses with expense_id"""
-        expenses_info = self.db_client.query(kind='Expense') \
-            .add_filter('id', '=', expense_id)
+
+        expenses_info = self.db_client.query(kind='Expenses')
+        expenses_key = self.db_client.key('Expenses', expenses_id)
+        expenses_info.key_filter(expenses_key, '=')
         expenses_data = expenses_info.fetch()
 
         if expenses_data:
-            return jsonify(expenses_data)
+            results = [{
+                'amount': ed['amount'],
+                'note': ed['note']
+            } for ed in expenses_data]
+            return jsonify(results)
         else:
             return make_response(jsonify(None), 204)
 
     def get_all_expenses(self):
         """Get JSON of all the expenses"""
-        expenses_info = self.db_client.query(kind='Expense')
+
+        expenses_info = self.db_client.query(kind='Expenses')
         expenses_data = expenses_info.fetch()
 
         if expenses_data:
             results = [{
-                'Title': ed.Title,
-                'Amount': ed.Amount
+                'amount': ed['amount'],
+                'note': ed['note']
             } for ed in expenses_data]
             return jsonify(results)
         else:
@@ -45,12 +52,12 @@ class ClaimExpenses:
 
     def add_expenses(self, data):
         """Add expense with given data amount and given data note"""
-        key = self.db_client.key('Expense')
+
+        key = self.db_client.key('Expenses')
         entity = datastore.Entity(key=key)
         entity.update({
-            'ID': entity.key.id_or_name,
-            'Amount': data['amount'],
-            'Note': data['note']
+            'amount': data.amount,
+            'note': data.note
         })
         self.db_client.put(entity)
         return make_response(jsonify(entity.key.id_or_name), 201)
@@ -101,7 +108,7 @@ def add_expense():  # noqa: E501
     """
     if connexion.request.is_json:
         form_data = FormData.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    return expense_instance.add_expenses(form_data)
 
 
 def delete_attachments_by_id():  # noqa: E501
@@ -123,7 +130,7 @@ def get_all_expenses():  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    return expense_instance.get_all_expenses()
 
 
 def get_attachments_by_id():  # noqa: E501
@@ -148,7 +155,7 @@ def get_document_by_id():  # noqa: E501
     return 'do some magic!'
 
 
-def get_expenses():  # noqa: E501
+def get_expenses(expenses_id):  # noqa: E501
     """Get information from expenses by id
 
      # noqa: E501
@@ -156,7 +163,7 @@ def get_expenses():  # noqa: E501
 
     :rtype: Expenses
     """
-    return 'do some magic!'
+    return expense_instance.get_expenses(expenses_id)
 
 
 def update_attachments_by_id():  # noqa: E501
