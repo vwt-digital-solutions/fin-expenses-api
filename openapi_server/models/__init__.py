21800b85f89d80d2b97186e89e31954f33e42c83
P. Mutale <peter@mutale.nl>
2019-07-25 09:03:50 +0200

    CodeFactorIssue: Fix Trailing Lines

--- openapi_server/models/__init__.py
+++ openapi_server/models/__init__.py
@@ -8,4 +8,3 @@ from openapi_server.models.cost_types import CostTypes
 from openapi_server.models.documents import Documents
 from openapi_server.models.expense_data import ExpenseData
 from openapi_server.models.expense_data_array import ExpenseDataArray
-
