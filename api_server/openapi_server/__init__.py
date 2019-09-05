864a330f9c28aa3f1c7046afb20c1a485729e1b7
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-09-05 10:39:31 +0200

    Add audit logging to API calls. DAT-688

--- api_server/openapi_server/__init__.py
+++ api_server/openapi_server/__init__.py
@@ -1,4 +1,6 @@
+import logging
 import connexion
+from flask import request, g
 from flask_cors import CORS
 
 from openapi_server import encoder
@@ -9,3 +11,28 @@ app.add_api('openapi.yaml',
             arguments={'title': 'P2P: Expenses API'},
             pythonic_params=True)
 CORS(app.app)
+
+
+@app.app.before_request
+def before_request():
+    g.user = ''
+    g.ip = ''
+
+
+@app.app.after_request
+def after_request_callback(response):
+    if 'x-appengine-user-ip' in request.headers:
+        g.ip = request.headers.get('x-appengine-user-ip')
+
+    logger = logging.getLogger('auditlog')
+    auditlog_list = list(filter(None, [
+        f"Request Url: {request.url}",
+        f"IP: {g.ip}",
+        f"User-Agent: {request.headers.get('User-Agent')}",
+        f"Response status: {response.status}",
+        f"UPN: {g.user}"
+    ]))
+
+    logger.info(' | '.join(auditlog_list))
+
+    return response
