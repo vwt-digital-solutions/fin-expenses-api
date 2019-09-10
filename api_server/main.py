b647780a6ff67ece97436f78bcbac6fdc62740ef
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-09-10 16:06:17 +0200

    Add cache control headers. DAT-768

--- api_server/main.py
+++ api_server/main.py
@@ -2,7 +2,28 @@ import openapi_server
 from flask import request, g
 import logging
 
+
+def make_no_cache_header():
+    def handle_hon_cache_header(resp):
+        if resp is not None and resp.headers is not None and resp.headers.get('Cache-Control'):
+            logging.info('Cache control headers already applied')
+            return resp
+
+        from werkzeug.datastructures import Headers, MultiDict
+        if (not isinstance(resp.headers, Headers)
+                and not isinstance(resp.headers, MultiDict)):
+            resp.headers = MultiDict(resp.headers)
+
+        # cache results for 5 minutes
+        # resp.headers.add('Cache-Control', 'max-age=300')
+        resp.headers.add('Cache-Control', 'no-cache')
+        return resp
+
+    return handle_hon_cache_header
+
+
 app = openapi_server.app
+app.after_request(make_no_cache_header())
 
 
 @app.app.before_request
