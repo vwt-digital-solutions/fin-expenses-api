df2b5698518807e6762231a7c254bd9c53186cbb
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-09-11 12:22:56 +0200

    AuditLog, extract to separate package. DAT-688

--- api_server/main.py
+++ api_server/main.py
@@ -1,38 +1,9 @@
 import openapi_server
-from flask import request, g
-import logging
 
 from cache_control import CacheControl
+from audit_log import AuditLog
 
 app = openapi_server.app
 
-
-@app.app.before_request
-def before_request():
-    g.user = ''
-    g.ip = ''
-    g.token = {}
-
-
-@app.app.after_request
-def after_request_callback(response):
-    if 'x-appengine-user-ip' in request.headers:
-        g.ip = request.headers.get('x-appengine-user-ip')
-
-    # enforce log level
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger('auditlog')
-    auditlog_list = list(filter(None, [
-        f"Request Url: {request.url}",
-        f"IP: {g.ip}",
-        f"User-Agent: {request.headers.get('User-Agent')}",
-        f"Response status: {response.status}",
-        f"UPN: {g.user}"
-    ]))
-
-    logger.info(' | '.join(auditlog_list))
-
-    return response
-
-
+AuditLog(app)
 CacheControl(app)
