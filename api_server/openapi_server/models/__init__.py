19fd1fab76f4ee712a1773fa9621f5a96376d19c
MaxvanderWeide <max.vanderweide@student.hu.nl>
2019-10-07 15:29:56 +0200

    Removed URL & URL Array

--- api_server/openapi_server/models/__init__.py
+++ api_server/openapi_server/models/__init__.py
@@ -9,5 +9,3 @@ from openapi_server.models.cost_types import CostTypes
 from openapi_server.models.expense_data import ExpenseData
 from openapi_server.models.expense_data_array import ExpenseDataArray
 from openapi_server.models.status import Status
-from openapi_server.models.url import Url
-from openapi_server.models.url_array import UrlArray
