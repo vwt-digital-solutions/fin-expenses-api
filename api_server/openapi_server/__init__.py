a55676f2948abfaab7712146bc6a5d84ca854c70
MaxvanderWeide <max.vanderweide@student.hu.nl>
2019-12-06 13:12:02 +0100

    Added limited validator to stop reflected errors DAT-1443

--- api_server/openapi_server/__init__.py
+++ api_server/openapi_server/__init__.py
@@ -1,9 +1,16 @@
 # import logging
+import logging
+
 import connexion
 # from flask import request, g
+from connexion import problem
+from connexion.decorators.validation import RequestBodyValidator
+from connexion.utils import is_null
 from flask_cors import CORS
+from jsonschema.validators import validator_for
 
 from openapi_server import encoder
+
 
 # try:
 #     import googleclouddebugger
@@ -12,11 +19,35 @@ from openapi_server import encoder
 # except ImportError:
 #     pass
 
+
+class LimitedRequestBodyValidator(RequestBodyValidator):
+
+    def validate_schema(self, data, url):
+        if self.is_null_value_valid and is_null(data):
+            return None
+
+        cls = validator_for(self.schema)
+        cls.check_schema(self.schema)
+        errors = tuple(cls(self.schema).iter_errors(data))
+
+        if errors:
+            logging.exception(f'Exception on {url} - LimitedRequestBodyValidator')
+            return problem(400, 'Bad Request', f'Some data is missing or incorrect at {url}', type='Validation')
+
+        return None
+
+
+validator_map = {
+    'body': LimitedRequestBodyValidator,
+}
+
 app = connexion.App(__name__, specification_dir='./openapi/')
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('openapi.yaml',
             arguments={'title': 'P2P: Expenses API'},
-            pythonic_params=True)
+            strict_validation=True,
+            validate_responses=True,
+            pythonic_params=True, validator_map=validator_map)
 CORS(app.app)
 
 # @app.app.before_request
