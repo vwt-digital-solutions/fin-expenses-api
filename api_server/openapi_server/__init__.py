5d0da3a5265b77bc8dbe1d4169d4ac34d238ec0c
dvanrhenen <d.vanrhenen@vwt.digital>
2020-02-06 11:23:08 +0100

    Did some SAST-refinement in code. DAT-1788

--- api_server/openapi_server/__init__.py
+++ api_server/openapi_server/__init__.py
@@ -1,26 +1,16 @@
-# import logging
 import logging
 from typing import AnyStr, Union
 
 import connexion
-# from flask import request, g
 from connexion import problem
 from connexion.decorators.validation import RequestBodyValidator
 from connexion.lifecycle import ConnexionResponse
-from connexion.utils import is_null, all_json
+from connexion.utils import is_null
 from flask_cors import CORS
 from jsonschema import ValidationError
-from jsonschema.validators import validator_for
 
 from openapi_server import encoder
 
-
-# try:
-#     import googleclouddebugger
-#
-#     googleclouddebugger.enable()
-# except ImportError:
-#     pass
 
 def validate_schema(self, data, url):
     # type: (dict, AnyStr) -> Union[ConnexionResponse, None]
@@ -53,28 +43,3 @@ app.add_api('openapi.yaml',
             arguments={'title': 'P2P: Expenses API'},
             strict_validation=True)
 CORS(app.app)
-
-# @app.app.before_request
-# def before_request():
-#     g.user = ''
-#     g.ip = ''
-#     g.token = {}
-#
-#
-# @app.app.after_request
-# def after_request_callback(response):
-#     if 'x-appengine-user-ip' in request.headers:
-#         g.ip = request.headers.get('x-appengine-user-ip')
-#
-#     logger = logging.getLogger('auditlog')
-#     auditlog_list = list(filter(None, [
-#         f"Request Url: {request.url}",
-#         f"IP: {g.ip}",
-#         f"User-Agent: {request.headers.get('User-Agent')}",
-#         f"Response status: {response.status}",
-#         f"UPN: {g.user}"
-#     ]))
-#
-#     logger.info(' | '.join(auditlog_list))
-#
-#     return response
