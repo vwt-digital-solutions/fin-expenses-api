8f3479e2dfcfa17b895c028e53b3ec5da8dd4a1a
dvanrhenen <d.vanrhenen@vwt.digital>
2020-03-17 12:05:15 +0100

    Double quoted to prevent globbing and word splitting. DAT-2251

--- api_server/generate.sh
+++ api_server/generate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
+docker run --rm -v "${PWD}":/local openapitools/openapi-generator-cli generate \
   --skip-overwrite \
   -i /local/openapi_server/openapi/openapi.yaml \
   -g python-flask \
