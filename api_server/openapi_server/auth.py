3ab5707575905b102b7b2522f4f943ab017389ef
dvanrhenen <d.vanrhenen@vwt.digital>
2020-04-23 19:18:37 +0200

    Added direct sa credentials request. DAT-2706

--- api_server/openapi_server/auth.py
+++ api_server/openapi_server/auth.py
@@ -1,23 +1,24 @@
+import config
+
 from google.auth import iam
 from google.auth.transport import requests
 from google.oauth2 import service_account
 
-TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
+TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'  # nosec
 
 
 def get_delegated_credentials(credentials, subject, scopes):
     try:
-        creds = credentials.with_subject(subject).with_scopes(scopes)
-    except AttributeError:
-
         request = requests.Request()
         credentials.refresh(request)
 
-        signer = iam.Signer(request, credentials,
-                            credentials.service_account_email)
+        signer = iam.Signer(request, credentials, config.GMAIL_SERVICE_ACCOUNT)
         creds = service_account.Credentials(
-            signer, credentials.service_account_email, TOKEN_URI,
-            scopes=scopes, subject=subject)
+            signer=signer,
+            service_account_email=config.GMAIL_SERVICE_ACCOUNT,
+            token_uri=TOKEN_URI,
+            scopes=scopes,
+            subject=subject)
     except Exception:
         raise
 
