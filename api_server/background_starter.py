786d055603b2785543220c59c82f2364406e45b5
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2020-09-24 12:08:00 +0200

    Reverted 763e7f20a853972de3c12584b1544613b37e50a4 and a8d23b5af7a68172fed075088cea44e0b00db6dd [skip ci]. DAT-4182

--- api_server/background_starter.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write('{"status":"OK"}')
-
-
-app = webapp2.WSGIApplication([('/_ah/start', MainPage)], debug=True)
