3c5060b87dd1239b923f1c09d678aae2206dc1f0
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2021-01-20 16:29:48 +0100

    Removed Cloud Logging until further research. DAT-5400

--- api_server/main.py
+++ api_server/main.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import openapi_server
-import google.cloud.logging
 
 from Flask_AuditLog import AuditLog
 from Flask_No_Cache import CacheControl
@@ -9,11 +8,6 @@ from flask_sslify import SSLify
 
 app = openapi_server.app
 flaskapp = app.app
-
-if 'GAE_INSTANCE' in os.environ:
-    client = google.cloud.logging.Client()
-    client.get_default_handler()
-    client.setup_logging()
 
 logging.basicConfig(level=logging.INFO)
 
