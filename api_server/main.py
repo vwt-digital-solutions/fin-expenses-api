c92e4fc1cbc7e015a8448cfd6e9966447da39f27
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2021-01-20 16:05:21 +0100

    Enabling Cloud Logging only for instances. DAT-5400

--- api_server/main.py
+++ api_server/main.py
@@ -10,13 +10,14 @@ from flask_sslify import SSLify
 app = openapi_server.app
 flaskapp = app.app
 
-logging.basicConfig(level=logging.INFO)
-
-client = google.cloud.logging.Client()
-client.get_default_handler()
-client.setup_logging()
 
 AuditLog(app)
 CacheControl(app)
 if 'GAE_INSTANCE' in os.environ:
     SSLify(app.app, permanent=True)
+
+    client = google.cloud.logging.Client()
+    client.get_default_handler()
+    client.setup_logging()
+
+logging.basicConfig(level=logging.INFO)
