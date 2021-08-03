c454ffd8e3f5b552d43a51d01e616cdd36fbf199
Mahmoud Aljabary <mahmoudaljabary@gmail.com>
2021-08-03 12:54:30 +0200

    Fixed DAT-7254

--- api_server/openapi_server/controllers/security_controller_.py
+++ api_server/openapi_server/controllers/security_controller_.py
@@ -1,21 +1,25 @@
-import config
 import logging
-from jwkaas import JWKaas
 
+import config
 from flask import g
+from jwkaas import JWKaas
 
 my_jwkaas = None
 my_e2e_jwkaas = None
 
-if hasattr(config, 'OAUTH_JWKS_URL'):
-    my_jwkaas = JWKaas(config.OAUTH_EXPECTED_AUDIENCE,
-                       config.OAUTH_EXPECTED_ISSUER,
-                       jwks_url=config.OAUTH_JWKS_URL)
+if hasattr(config, "OAUTH_JWKS_URL"):
+    my_jwkaas = JWKaas(
+        config.OAUTH_EXPECTED_AUDIENCE,
+        config.OAUTH_EXPECTED_ISSUER,
+        jwks_url=config.OAUTH_JWKS_URL,
+    )
 
-if hasattr(config, 'OAUTH_E2E_JWKS_URL'):
-    my_e2e_jwkaas = JWKaas(config.OAUTH_E2E_EXPECTED_AUDIENCE,
-                           config.OAUTH_E2E_EXPECTED_ISSUER,
-                           jwks_url=config.OAUTH_E2E_JWKS_URL)
+if hasattr(config, "OAUTH_E2E_JWKS_URL"):
+    my_e2e_jwkaas = JWKaas(
+        config.OAUTH_E2E_EXPECTED_AUDIENCE,
+        config.OAUTH_E2E_EXPECTED_ISSUER,
+        jwks_url=config.OAUTH_E2E_JWKS_URL,
+    )
 
 
 def info_from_oAuth2(token):
@@ -34,12 +38,18 @@ def info_from_oAuth2(token):
 
     if result is None and my_e2e_jwkaas is not None:
         token_info = my_e2e_jwkaas.get_connexion_token_info(token)
-        if token_info is not None and 'appid' in token_info and token_info['appid'] == config.OAUTH_E2E_APPID:
-            logging.warning('Approved e2e access token for appid [%s]', token_info['appid'])
+        if (
+            token_info is not None
+            and "appid" in token_info
+            and token_info["appid"] == config.OAUTH_E2E_APPID
+        ):
+            logging.warning(
+                "Approved e2e access token for appid [%s]", token_info["appid"]
+            )
             result = config.e2e_key_data
 
     if result is not None:
-        g.user = result.get('upn', '')
+        g.user = result.get("upn", "").strip()
         g.token = result
 
     return result
